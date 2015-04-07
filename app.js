57e7b2acca822e0377012773bb5795ad9b9cc502
pdedes <dedes1821@gmail.com>
2015-04-06 21:56:27 -0400

    Fixed card formatting of data. Added a capitalization filter. Added an indeterminate spinner.

--- app.js
+++ app.js
@@ -9,11 +9,13 @@
 			var countryCode = 'us';
 
 			$scope.weatherData = null;
+			$scope.loader = true;
 
 			$scope.searchZip = function () {
 				console.log("entered zip");
 				$scope.weatherData = null;
-				
+				$scope.loader = false;
+
 				$http({
 					method: "GET",
 					url: api_root,
@@ -22,6 +24,7 @@
 					}
 				}).success(function(data) {
 					console.log('Data retrieved', data);
+					$scope.loader = true;
 					$scope.weatherData = data;
 				}).catch(function (err) {
 					console.log('Error Catch Hit');
@@ -37,6 +40,14 @@
 			// 	});
 			// };
 
-		}]);
+		}])
+
+		.filter('capitalize', function() {
+		  return function(input, scope) {
+		    if (input!=null)
+		    input = input.toLowerCase();
+		    return input.substring(0,1).toUpperCase()+input.substring(1);
+		  }
+		});
 
 })();
